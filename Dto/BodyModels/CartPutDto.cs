789d4a8d01d2fce91b948505ccabbbe3b773714b
Arr404 <agilfuad@gmail.com>
2024-05-13 20:36:27 +0700

    fix push put Database transfer object

--- Dto/BodyModels/CartPutDto.cs
+++ Dto/BodyModels/CartPutDto.cs
@@ -5,7 +5,6 @@ namespace Pasar_Maya_Api.Dto.BodyModels
 {
     public class CartPutDto
     {
-        public int Id { get; set; }
         public string UserId { get; set; }
         public int GroupId { get; set; }
         public ICollection<ProductQuantityDto> ProductQuantities { get; set; }
--- Dto/BodyModels/MarketPostDto.cs
+++ Dto/BodyModels/MarketPostDto.cs
@@ -4,7 +4,6 @@ namespace Pasar_Maya_Api.Dto.BodyModels
 {
     public class MarketPostDto
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
